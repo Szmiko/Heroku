7b3751a054e1ad29b4a112cb9e70c9e068fc568d
Szymon Grabowski <szmiko@wp.pl>
2018-06-16 17:44:39 +0200

    Task

--- server2.js
+++ server2.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongo ds259410.mlab.com:59410/kodilla -u <Szmiko> -p <DOheroku8*>', {});
+mongoose.connect('mongodb://localhost/nodeappdatabase', {});
 
 //new user Schema
 const userSchema = new Schema({
